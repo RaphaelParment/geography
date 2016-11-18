49c10245652d5fb9232f0f8ba9b64ea5ccfe4611
Raphael Parment <raphparment@gmail.com>
2016-11-18 20:29:29 +0100

    The data.

---
+++ data/regions.sql
@@ -0,0 +1,26 @@
+INSERT INTO `region` (`name`, `capital_id`, `area`, `population`) VALUES 
+('Auvergne-Rhone-Alpes',,69711,7874586),
+('Bourgogne-Franche-Comté',,47784,2821042),
+('Bretagne',,27208,3294302),
+('Centre-Val-de-Loire',,39151,2582374),
+('Corse',,8680,326898),
+('Grand-Est',,57433,5560405),
+('Guadeloupe',,1628,400132),
+('Guyane',,83846,254541),
+('Hauts-de-France',,31913,6006853),
+('Ile-de-France',,12011,12073914),
+('La Réunion',,2504,843529),
+('Martinique',,1128,378243),
+('Mayote',,376,226915),
+('Normandie',,29906,3334657),
+('Nouvelle-Aquitaine',,84061,5904843),
+('Occitanie',,72724,5791865),
+('Pays-de-la-Loire',,32082,3716068),
+('Provence-Alpes-Cote-D\'Azure',,31400,4989435),
+
+
+
+
+
+
+
